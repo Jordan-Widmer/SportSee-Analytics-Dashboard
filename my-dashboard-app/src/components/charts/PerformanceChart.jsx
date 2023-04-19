5499ba16d02e660d6143ec4c9d4ed2725a0e0eca
Lord Shaxx <87854378+Korootak@users.noreply.github.com>
2023-04-19 17:44:53 +0200

    Refactored RadarChartComponent into PerformanceChart with a custom kind mapping and updated tooltip

--- my-dashboard-app/src/components/charts/PerformanceChart.jsx
+++ my-dashboard-app/src/components/charts/PerformanceChart.jsx
@@ -1,5 +1,34 @@
 import React, { useRef, useEffect } from 'react';
-import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import {
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  Radar,
+  ResponsiveContainer,
+  Legend,
+  Tooltip,
+} from 'recharts';
+
+const CustomTooltip = ({ active, payload, label }) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip">
+        <p className="label">{`${label}: ${payload[0].value}`}</p>
+      </div>
+    );
+  }
+  return null;
+};
+
+const kind = {
+  1: 'cardio',
+  2: 'energy',
+  3: 'endurance',
+  4: 'strength',
+  5: 'speed',
+  6: 'intensity'
+};
 
 const PerformanceChart = ({ data }) => {
   const chartRef = useRef(null);
@@ -10,15 +39,28 @@ const PerformanceChart = ({ data }) => {
     }
   }, [chartRef]);
 
+  const formattedData = Object.values(kind).map((name, index) => ({
+    name,
+    value: data[index] ? data[index].value : 0,
+  }));
+
   return (
     <div ref={chartRef} style={{ width: '100%', height: 300 }}>
       <ResponsiveContainer>
-        <BarChart data={data}>
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Bar dataKey="value" fill="#8884d8" />
-        </BarChart>
+        <RadarChart cx="50%" cy="50%" outerRadius="80%" data={formattedData}>
+          <PolarGrid />
+          <PolarAngleAxis dataKey="name" stroke="#000" />
+          <PolarRadiusAxis angle={30} domain={[0, 'auto']} tick={false} />
+          <Radar
+            name="User Performance"
+            dataKey="value"
+            stroke="#8884d8"
+            fill="#8884d8"
+            fillOpacity={0.6}
+          />
+          <Tooltip content={<CustomTooltip />} />
+          <Legend />
+        </RadarChart>
       </ResponsiveContainer>
     </div>
   );
