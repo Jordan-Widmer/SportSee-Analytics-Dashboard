3a7654991e57cd07d86f4424a485e9ff629ea0b6
Jordan WIDMER <jordanwidmer78@gmail.com>
2024-01-07 05:42:34 +0100

    Add Back-End Data and Route Handling Implement Back-End Data Management and API Routes
    - Integrated mock data for user information, activity, sessions, performance, and daily goal completion.
    - Set up server with Express.js and CORS for handling API requests.
    - Created route handlers to fetch user-specific data like activity, average sessions, performance, and daily goal completion.
    
    Note: This implementation is crucial for the application's data management and provides a foundation for front-end integration.
    
    Resolves: #XYZ (Back-End Data Management and API Route Setup)

---
+++ back-end/app/routes.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const idx = require('idx');
+
+const router = express.Router();
+
+const {
+  getUserById,
+  getUserActivityById,
+  getUserAverageSession,
+  getUserPerformance,
+  getUserDailyGoalCompletion,
+} = require('./models');
+
+const {
+  handleNoUserData,
+} = require('./middleware');
+
+router.get('/user/:id', (req, res) => {
+  const userId = idx(req, _ => _.params.id);
+  const userData = getUserById(Number(userId));
+
+  return handleNoUserData(res, userData);
+});
+
+router.get('/user/:id/activity', (req, res) => {
+  const userId = idx(req, _ => _.params.id);
+  const userData = getUserActivityById(Number(userId));
+
+  return handleNoUserData(res, userData);
+});
+
+router.get('/user/:id/average-sessions', (req, res) => {
+  const userId = idx(req, _ => _.params.id);
+  console.log('userId:', userId);
+  const userData = getUserAverageSession(Number(userId));
+  console.log('userData:', userData);
+
+  if (userData === null) {
+    res.status(404).json({ message: 'User not found' });
+  } else {
+    res.json(userData);
+  }
+});
+
+router.get('/user/:id/performance', (req, res) => {
+  const userId = idx(req, _ => _.params.id);
+  const userData = getUserPerformance(Number(userId));
+
+  return handleNoUserData(res, userData);
+});
+
+router.get('/user/:id/daily-goal-completion', async (req, res) => {
+  const userId = idx(req, _ => _.params.id);
+  const userData = await getUserDailyGoalCompletion(Number(userId));
+
+  return handleNoUserData(res, userData);
+});
+
+module.exports = router;
